e318c1d878e70bf2c8258987ed407f9c045f1549
Your Name <you@example.com>
2020-05-15 00:07:14 -0300

    appJuego login ok

---
+++ appJuego/app/src/main/java/com/example/appjuego/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.appjuego
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_contador.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnIr.setOnClickListener {
+            val intent: Intent =Intent(this, QuienSoy::class.java)
+            startActivity(intent)
+
+            }
+
+        }
+    }
+}
---
+++ appJuego/app/src/main/java/com/example/appjuego/contador.kt
@@ -0,0 +1,14 @@
+package com.example.appjuego
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class contador : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contador)
+
+    }
+}
