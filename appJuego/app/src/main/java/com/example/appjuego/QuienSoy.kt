d7f4c8f307a198b70bdc0e4bd48f104e11d44093
Your Name <you@example.com>
2020-05-18 17:41:19 -0300

    navegacion final

--- appJuego/app/src/main/java/com/example/appjuego/QuienSoy.kt
+++ appJuego/app/src/main/java/com/example/appjuego/QuienSoy.kt
@@ -1,12 +1,19 @@
 package com.example.appjuego
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_quien_soy.*
 
 class QuienSoy : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quien_soy)
+        btn_next.setOnClickListener {
+            val intent: Intent =Intent(this, contador::class.java)
+            startActivity(intent)
+
+        }
     }
 }
